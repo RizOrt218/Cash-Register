b990f19763cb26cd3f9a298b20a4c82455b9ce35
RizOrt218 <rizhiafalen@gmail.com>
2015-12-05 11:32:42 -1000

    WIP! might have to redo :/

---
+++ calculator.js
@@ -0,0 +1,18 @@
+var calculator = (function(){
+
+var numInput;
+var total;
+
+function clickButton(number){
+  return getElementById('btn7');
+}
+
+  var displayValue = function(){
+    var button = document.getElementById('number').value;
+
+      };
+
+
+}());
+
+
